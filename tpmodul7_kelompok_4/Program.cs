ac6671c25dd51949a4dae86fd40caad63c31386e
Muhammad Haulul Azkiyaa <haululazkiya@gmail.com>
2024-03-31 15:29:32 +0700

    commit pertama

---
+++ tpmodul7_kelompok_4/Program.cs
@@ -0,0 +1,20 @@
+﻿/* 
+ 
+KELOMPOK 4 (TIM SITU)
+
+ANGOTA:
+Fauzein Mulya Warman / 1302223127
+Iksan Oktav Risandy / 1302223042
+Muhammad Dias Adani / 1302223134
+Muhammad Haulul Azkiyaa / 1302223007
+Muhammad Nur Shodiq / 1302223054
+
+*/
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
