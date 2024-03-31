2f76c76fbdfd6b9d54e003c06c1cebf3dd6c70a3
Muhammad Haulul Azkiyaa <haululazkiya@gmail.com>
2024-03-31 22:40:47 +0700

    menambahkan class DataMahasiswa1302223007

--- tpmodul7_kelompok_4/Program.cs
+++ tpmodul7_kelompok_4/Program.cs
@@ -11,10 +11,14 @@ Muhammad Nur Shodiq / 1302223054
 
 */
 
+using tpmodul7_kelompok_4;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        DataMahasiswa1302223007 mahasiswa = new DataMahasiswa1302223007();
+        mahasiswa.ReadJSON();
+
     }
 }
